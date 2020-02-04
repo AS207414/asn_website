7347caa4d12986f0cf6eb8c7866ac771fb932339
Ganawa Juanah <ganawa@juanah.com>
2020-02-03 18:55:37 -0600

    added files from local copy

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def home_page():
+    return render_template('index.html', users_ip=request.remote_addr)
+
+
+@app.route('/peering.html', methods=["GET"])
+def peering():
+    return render_template('peering.html', users_ip=request.remote_addr)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
