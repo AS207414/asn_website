013e607341b69ca4e387a3484d402b89a317eb6b
Ganawa Juanah <ganawa@juanah.com>
2020-06-11 16:17:49 -0500

    added 404 and 500 pages

--- app/main.py
+++ app/main.py
@@ -11,3 +11,18 @@ def home_page():
 @app.route('/peering.html', methods=["GET"])
 def peering():
     return render_template('peering.html')
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
